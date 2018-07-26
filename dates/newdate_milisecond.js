d1d50a51cb0434b9aa4b9c473f91e0c74ffbfbe8
vnhai <haingocvu@hotmail.com>
2018-07-26 21:55:52 +0700

    date get methods

---
+++ dates/newdate_milisecond.js
@@ -0,0 +1,2 @@
+let newdate = new Date(24 * 60 * 60 * 1000);
+console.log(newdate)
---
+++ dates/time_get_methods.js
@@ -0,0 +1,21 @@
+let _date = new Date();
+let _time = _date.getTime();
+console.log(_time)
+console.log(_date.getFullYear())
+console.log(_date.getMonth())
+console.log(_date.getDate())
+console.log(_date.getHours())
+console.log(_date.getMinutes())
+console.log(_date.getSeconds())
+console.log(_date.getMilliseconds())
+console.log(_date.getDay())
+//gettime trả về số milisecond từ 1/1/1970
+//getfullyear trả về yyyy
+//getmonth trả về month từ 0-11 (0-january, 11-december)
+//getdate trả về date từ 1-31
+//gethour trả về hour từ 0-23
+//getminutes trả về minutes từ 0-59
+//getsecond trả về second từ 0-59
+//getmilisecond trả về từ 0-999
+//getDay trả về date từ 0-6 (tương ứng thứ cn-t7)
+
